577c76816eb1ee2928689272fb333cdf5de5ef7c
i.kłusek <J6ZkqwVUn>
2023-05-06 19:45:12 +0200

    Account Profile Sumamry UI | flex and LN-elements mixin

---
+++ linkedin-frontend/src/app/home/components/start-post/start-post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-post',
+  templateUrl: './start-post.component.html',
+  styleUrls: ['./start-post.component.sass']
+})
+export class StartPostComponent {
+
+}
