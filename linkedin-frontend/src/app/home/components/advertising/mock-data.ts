f345ccb74977c2e94b6f00e0ee937f797849fc6b
i.kłusek <J6ZkqwVUn>
2023-05-06 23:20:38 +0200

    Advertising UI

---
+++ linkedin-frontend/src/app/home/components/advertising/mock-data.ts
@@ -0,0 +1,22 @@
+interface advertData {
+  image: string;
+  name: string;
+  position: string;
+}
+export const data: advertData[] = [
+  {
+    image: 'advert/github.JPG',
+    name: 'GitHub',
+    position: 'Firma, Oprogramowanie komputerowe',
+  },
+  {
+    image: 'advert/google.JPG',
+    name: 'Google',
+    position: 'Firma, Oprogramowanie komputerowe',
+  },
+  {
+    image: 'advert/HRejterzy.JPG',
+    name: 'HRejterzt',
+    position: 'CodeTwo, JSON at HRejterzy',
+  },
+];
