e0d702a5b882fdeaa36aa9d97049ba500e799a95
i.kłusek <J6ZkqwVUn>
2023-05-07 16:15:45 +0200

    Account Start Post Modal UI

--- linkedin-frontend/src/app/home/components/start-post/data.ts
+++ linkedin-frontend/src/app/home/components/start-post/data.ts
@@ -1,11 +1,12 @@
 interface optionsInterface {
   icon: string;
   text: string;
+  color: string;
 }
 
 export const options: optionsInterface[] = [
-  { icon: 'photo_camera', text: 'Zdjęcie' },
-  { icon: 'smart_display', text: 'Wideo' },
-  { icon: 'event_note', text: 'Wydarzenia' },
-  { icon: 'edit_note', text: 'Napisz artykuł' },
+  { icon: 'photo_camera', text: 'Zdjęcie', color: '#378fe9' },
+  { icon: 'smart_display', text: 'Wideo', color: '#5f9b41' },
+  { icon: 'event_note', text: 'Wydarzenia', color: '#c37d16' },
+  { icon: 'edit_note', text: 'Napisz artykuł', color: '#e16745' },
 ];
