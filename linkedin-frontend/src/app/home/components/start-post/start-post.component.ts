a7f86f1f773d2e0ece94b037c5a38ff9a548e936
i.kłusek <J6ZkqwVUn>
2023-05-07 11:46:18 +0200

    Start Post UI

--- linkedin-frontend/src/app/home/components/start-post/start-post.component.ts
+++ linkedin-frontend/src/app/home/components/start-post/start-post.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { options } from './data';
 
 @Component({
   selector: 'app-start-post',
   templateUrl: './start-post.component.html',
-  styleUrls: ['./start-post.component.sass']
+  styleUrls: ['./start-post.component.sass'],
 })
 export class StartPostComponent {
-
+  options = options;
 }
