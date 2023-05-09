824a18273f669afa540ecb3626ab6c5c188284fc
i.kłusek <J6ZkqwVUn>
2023-05-09 20:07:29 +0200

    Post UI

--- linkedin-frontend/src/app/home/components/all-posts/all-posts.component.ts
+++ linkedin-frontend/src/app/home/components/all-posts/all-posts.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { data } from './data';
 
 @Component({
   selector: 'app-all-posts',
   templateUrl: './all-posts.component.html',
-  styleUrls: ['./all-posts.component.sass']
+  styleUrls: ['./all-posts.component.sass'],
 })
 export class AllPostsComponent {
-
+  options = data;
 }
