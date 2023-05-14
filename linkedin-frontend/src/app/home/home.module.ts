4556e653064e4d736ea560bd7e56382f5eeaca8d
i.kłusek <J6ZkqwVUn>
2023-05-14 21:33:17 +0200

    Separate routing modules

--- linkedin-frontend/src/app/home/home.module.ts
+++ linkedin-frontend/src/app/home/home.module.ts
@@ -4,25 +4,13 @@ import { StartPostComponent } from './components/start-post/start-post.component
 import { AdvertisingComponent } from './components/advertising/advertising.component';
 import { HomeComponent } from './home.component';
 import { AllPostsComponent } from './components/all-posts/all-posts.component';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { FiltersComponent } from './components/filters/filters.component';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HeaderComponent } from '../header/header.component';
-import { PopoverComponent } from '../header/popover/popover.component';
-import { ModalComponent } from './components/start-post/modal/modal.component';
-import { SearchbarComponent } from '../header/searchbar/searchbar.component';
-import { AppRoutingModule } from '../app-routing.module';
-import { HeaderModule } from '../header/header.module';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
-  imports: [
-    HeaderModule,
-    SharedModule,
-    ReactiveFormsModule,
-    InfiniteScrollModule,
-    AppRoutingModule,
-  ],
+  imports: [SharedModule, ReactiveFormsModule, InfiniteScrollModule],
   declarations: [
     HomeComponent,
     ProfileSummaryComponent,
