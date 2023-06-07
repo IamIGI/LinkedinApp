d2e1dbb09c7c054bc1d6abc8f59ec35dbd4199bb
i.kłusek <J6ZkqwVUn>
2023-06-07 14:05:00 +0200

    Adding Image from startPost options, added notification wheu user add image

--- linkedin-frontend/src/app/home/services/post.service.ts
+++ linkedin-frontend/src/app/home/services/post.service.ts
@@ -2,15 +2,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from '../models/Post';
 import { environment } from 'src/environments/environment.development';
-import { map, take, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  map,
+  take,
+  tap,
+  Observable,
+  of,
+  switchMap,
+} from 'rxjs';
 import { AuthService } from 'src/app/guests/components/auth/services/auth.service';
+import { CreatePost } from '../components/start-post/modal/modal.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
+  private postImage$ = new BehaviorSubject<boolean>(false);
+  postBody: CreatePost = {
+    content: '',
+    role: '',
+    file: undefined,
+  };
+
   postURL = `${environment.baseApiUrl}/feed`;
   postURLwithImage = `${environment.baseApiUrl}/feed/image`;
+
   constructor(private http: HttpClient, private authService: AuthService) {
     this.authService
       .getUserImageName()
@@ -39,19 +56,51 @@ export class PostService {
     return this.http.get<Post[]>(modifiedURL);
   }
 
-  createPost(formData: FormData) {
-    const postWithImage = formData.get('file') instanceof File;
+  isPostImageAdded(): Observable<boolean> {
+    return this.postImage$.asObservable();
+    // return of(Boolean(this.postBody.file));
+  }
+
+  setPostImage(file: File) {
+    this.postBody.file = file;
+    this.postImage$.next(true);
+  }
+
+  setPostBody(body: CreatePost) {
+    this.postBody.content = body?.content;
+    this.postBody.role = body?.role;
+    console.log(this.postBody);
+  }
+
+  clearPostBody() {
+    this.postBody = {
+      content: '',
+      role: '',
+      file: undefined,
+    };
+    this.postImage$.next(false);
+  }
+
+  createPost() {
+    let formValues = new FormData();
+    formValues.append('content', this.postBody.content);
+    formValues.append('file', this.postBody.file as File);
+    //In future add there roles
+
+    const postWithImage = formValues.get('file') instanceof File;
     if (postWithImage) {
-      return this.http.post<Post>(this.postURLwithImage, formData).pipe(
+      return this.http.post<Post>(this.postURLwithImage, formValues).pipe(
         take(1),
         map((result: Post) => {
+          this.clearPostBody();
           return result;
         })
       );
     }
-    return this.http.post<Post>(this.postURL, formData).pipe(
+    return this.http.post<Post>(this.postURL, formValues).pipe(
       take(1),
       map((result: Post) => {
+        this.clearPostBody();
         return result;
       })
     );
